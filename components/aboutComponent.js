b0c97c0086039ea59f5e4b88473c7f7c3df7f14f
Robert <rfullerton07@gmail.com>
2021-01-08 23:56:54 -0800

    added loading and error message indicators

--- components/aboutComponent.js
+++ components/aboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView , FlatList} from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -21,10 +22,7 @@ const Mission = () => {
 };
 
 class About extends Component {
-
     
-       
-
     static navigationOptions = {
         title: 'About Us',
     }
@@ -39,17 +37,39 @@ class About extends Component {
                 );
             };
 
-        return (
-            <ScrollView>
-                <Mission />
-            <Card title="Community Partners">
-            <FlatList 
-                data={this.props.partners.partners}
-                renderItem={renderPartner}
-                keyExtractor={item => item.id.toString()}
-        />
-            </Card>
-            </ScrollView>
+            if (this.props.partners.isLoading) {
+                return (
+                    <ScrollView>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Loading />
+                        </Card>
+                    </ScrollView>
+                );
+            }
+            if (this.props.partners.errMess) {
+                return (
+                    <ScrollView>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                            <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                    </ScrollView>
+                );
+            }
+            return (
+                <ScrollView>
+                    <Mission />
+                <Card title="Community Partners">
+                <FlatList 
+                    data={this.props.partners.partners}
+                    renderItem={renderPartner}
+                    keyExtractor={item => item.id.toString()}
+                />
+                </Card>
+                </ScrollView>
         );
     }
 }
